41a627747ce6e6ecbc48dfd6219f7ca43f99a38e
Cristian Mayco da Costa <cristianmayco@gmail.com>
2021-11-17 15:26:19 -0300

    add consign e middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const app = require("express")();
-const bodyParser = require('body-parser');
+const consign = require("consign");
 
-app.use(bodyParser.json())
-
+consign({ cwd: "src", verbose: false })
+  .include("./config/middlewares.js")
+  .into(app);
 
 app.get("/", (req, res) => {
   res.status(200).send();
@@ -18,8 +19,8 @@ app.get("/users", (req, res) => {
   res.status(200).json(users);
 });
 
-app.post('/users', (req, res) =>{
-    res.status(201).json(req.body);
-})
+app.post("/users", (req, res) => {
+  res.status(201).json(req.body);
+});
 
 module.exports = app;
---
+++ src/config/middlewares.js
@@ -0,0 +1,7 @@
+const bodyParser = require('body-parser');
+const app = require('../app');
+
+module.exports = (app)=>{
+    app.use(bodyParser.json())
+};
+
